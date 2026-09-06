c9564594d6f0888b5d63b73ce494d1764e49e169
Yash Sahu <sahuyash2192@gmail.com>
2026-09-07 03:49:18 +0530

    Time: 6 ms (44.11%), Space: 44.6 MB (59.84%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        int count=0;
+        int ch[]=new int[26];
+        if(s.length()!=t.length())return false;
+        for(int i=0;i<s.length();i++){
+            ch[s.charAt(i)-'a']++;
+            ch[t.charAt(i)-'a']--;
+        }
+        for(int j=0;j<ch.length;j++){
+            if(ch[j]!=0)return false;
+        }
+   
+    return true;
+    }
+}
